b5dc6220da84c92f34a1949faa7cf1e1ff039adc
supasorn <supasorn@gmail.com>
2019-10-24 11:00:36 +0700

    updated tasklauncher.py

--- tasklauncher.py
+++ tasklauncher.py
@@ -5,34 +5,40 @@ from multiprocessing import Pool
 from functools import partial
 import sys
 
+src = os.environ["tl_src"]
+target = os.environ["tl_target"]
+venv = os.environ["tl_venv"]
+
 
 session_special = "TL"
-clusters = ["v2", "v3", "v4"]
-src = "supasorn@10.204.162.213:/home2/research/orbiter"
-target = "/home/supasorn/mnt/orbiter"
-venv = "source /home/vll/venv_tf1.14/bin/activate"
+
+if "tl_clusters" not in os.environ:
+  clusters = ["v2", "v3", "v4"]
+else:
+  clusters = os.environ["tl_clusters"].split(",")
 
 
 def cmd(a):
   print("  " + a)
   os.system(a)
 
-def getWindowList(cluster):
-  stdout, stderr = Popen(['ssh', cluster, 'tmux list-windows -t ' + session_special], stdout=PIPE).communicate()
-  if "[" not in stdout:
-    return None
-  k = [x.split(" ")[1] for x in stdout.rstrip().split("\n")]
-  k = [x[:-1] if x[-1] == '-' or x[-1] == '*' else x for x in k]
-  return k
 
-def printGPU(cluster):
+def showGPUs_fn(cluster):
   return "Cluster " + cluster + "\n" + GPUtil.showUtilization(ssh=cluster)
 
 def showGPUs():
   p = Pool(len(clusters))
-  a = p.map(printGPU, clusters)
+  a = p.map(showGPUs_fn, clusters)
   for x in a:
     print(x)
+
+def getWindowList(cluster):
+  stdout, stderr = Popen(['ssh', cluster, 'tmux list-windows -t ' + session_special], stdout=PIPE).communicate()
+  if "[" not in stdout:
+    return []
+  k = [x.split(" ")[1] for x in stdout.rstrip().split("\n")]
+  k = [x[:-1] if x[-1] == '-' or x[-1] == '*' else x for x in k]
+  return k
 
 def showWindows():
   p = Pool(len(clusters))
@@ -40,6 +46,15 @@ def showWindows():
   for i, x in enumerate(a):
     print("Cluster " + clusters[i] + "\n  " + str(x) + "\n")
 
+
+def getAvailableGPUs_fn(cluster):
+  return (cluster, GPUtil.getAvailable(cluster, limit=4))
+
+def getAvailableGPUs():
+  p = Pool(len(clusters))
+  gpu_list = p.map(getAvailableGPUs_fn, clusters)
+  gpu_list.sort(key=lambda x:len(x[1]), reverse=True)
+  return (gpu_list[0][0], gpu_list[0][1][0]) # cluster, gpu_id
 
 def main():
   if sys.argv[1] == "ls":
@@ -54,45 +69,55 @@ def main():
       exit()
 
     sp = sys.argv[1].split("@")
-    session_name = sp[0]
 
     code = sp[1]
-    cluster = ""
-    for c in clusters:
-      if code[:len(c)] == c:
-        cluster = c
-        code = code[len(c):]
-        break
-
-    if cluster == "":
-      print("Invalid cluster")
-      exit()
-    if code != "" and (code[0] != 'g' or code[1] not in ["0", "1", "2", "3", "a"]):
-      print("Invalid GPU code")
-      exit()
-
-    # auto
-    if code == "" or code[1] == "a":
-      gpu_id = str(GPUtil.getFirstAvailable(cluster)[0])
+    if code == "": #automatically select cluster and gpu
+      print("Finding a free cluster and a free gpu...")
+      cluster, gpu_id = getAvailableGPUs()
+      gpu_id=str(gpu_id)
     else:
-      gpu_id = code[1]
+      cluster = ""
+      for c in clusters:
+        if code[:len(c)] == c:
+          cluster = c
+          code = code[len(c):]
+          break
 
-    print(gpu_id)
+      if cluster == "":
+        print("Invalid cluster")
+        exit()
+      if code != "" and (code[0] != 'g' or code[1] not in ["0", "1", "2", "3", "a"]):
+        print("Invalid GPU code")
+        exit()
+
+      # auto
+      if code == "" or code[1] == "a":
+        print("Finding a free gpu...")
+        gpu_id = str(GPUtil.getFirstAvailable(cluster)[0])
+      else:
+        gpu_id = code[1]
 
     print("Using cluster: " + cluster)
     print("Using gpu: " + gpu_id)
+
+    print("Establishing session name...")
+    windows = getWindowList(cluster)
+    if sp[0] == "":
+      sp[0] = "sess00"
+      while len(windows) and sp[0] in windows:
+        sp[0] = "sess%02d" % (int(sp[0][4:])+1)
+    session_name = sp[0]
     print("Session name: " + session_name)
+
 
     print("SSHFS Mapping ...")
     sshfs_cmd = "ssh " + cluster + " -t \"mkdir -p " + target + "; nohup sshfs -o cache=no -o IdentityFile=/home/supasorn/.ssh/id_rsa " + src + " " + target + "\""
     cmd(sshfs_cmd)
 
     tf_cmd = "CUDA_VISIBLE_DEVICES=" + gpu_id + " " + " ".join(sys.argv[2:])
+    terminal_cmd = venv + "; cd " + target + "; " + tf_cmd + "; exit"
 
-    terminal_cmd = venv + "; cd " + target + "; " + tf_cmd
-
-    windows = getWindowList(cluster)
-    if windows is None:
+    if len(windows) == 0:
       tmux_creation = 'tmux new -A -s ' + session_special + ' -n ' + session_name + '\;'
     elif session_name in windows:
       print("Duplicate session name")
