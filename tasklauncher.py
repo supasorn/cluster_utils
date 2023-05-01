1b310df2057f0af4c3fd34228964ba3a38ab829a
Supasorn Suwajanakorn <supasorn@gmail.com>
2023-05-01 11:38:31 +0700

    auto commit

--- tasklauncher.py
+++ tasklauncher.py
@@ -1,5 +1,6 @@
 '''
-alias run="python .../cluster_utils/tasklauncher.py"
+add this alias to bashrc or zshrc
+alias run="python YOUR_PATH/tasklauncher_uni.py"
 
 List gpu usage:
   run lsgpu
@@ -16,12 +17,15 @@ Launch a job on a particular cluster (e.g., on v3):
 Launch a job on a particular cluster on a particular gpu:
   run @v3g0 [cmd]
 
+Launch a job on a particular cluster using 4 gpus:
+  run @v3#4 [cmd]
+
 Launch a job with a specific session name:
   run name@v3g0 [cmd]
 '''
 
 import os
-import GPUtil
+from GPUtil import *
 from subprocess import Popen, PIPE
 from multiprocessing import Pool
 from functools import partial
@@ -29,17 +33,18 @@ import sys
 import socket
 import getpass
 import platform
+import random
 
-session_special = "TL"
+session_special = "UL"
 
-if "tl_clusters" not in os.environ:
-  clusters = ["v1", "v2", "v3", "v4"]
+if "clusters" not in os.environ:
+  clusters = ["v%d" % i for i in range(1, 24)]
+  print(clusters)
 else:
-  clusters = os.environ["tl_clusters"].split(",")
+  clusters = os.environ["clusters"].split(",")
 
 if "tl_venv" not in os.environ:
-  # venv = "source /home/vll/venv_tf1.15/bin/activate"
-  venv = "source /home/vll/venv_pytorch/bin/activate"
+  venv = "source /home/vll/venv_pytorch1.9/bin/activate"
 else:
   venv = os.environ["tl_venv"]
 
@@ -61,16 +66,17 @@ def get_ip():
   return IP
 
 def showGPUs_fn(cluster):
-  return "Cluster " + cluster + "\n" + GPUtil.showUtilization(ssh=cluster)
+  return showUtilization(cluster)
 
 def showGPUs():
   p = Pool(len(clusters))
   a = p.map(showGPUs_fn, clusters)
+  print("")
   for x in a:
     print(x)
 
-def getWindowList(cluster):
-  stdout, stderr = Popen(['ssh', cluster, 'tmux list-windows -t ' + session_special], stdout=PIPE).communicate()
+def getWindowList():
+  stdout, stderr = Popen(['tmux list-windows -t ' + session_special], stdout=PIPE, shell=True).communicate()
   stdout = str(stdout)
   if "[" not in stdout:
     return []
@@ -82,20 +88,39 @@ def getWindowList(cluster):
   return k
 
 def showWindows():
-  p = Pool(len(clusters))
-  a = p.map(getWindowList, clusters)
+  # p = Pool(len(clusters))
+  a = getWindowList() #p.map(getWindowList)
   for i, x in enumerate(a):
-    print("Cluster " + clusters[i] + "\n  " + str(x) + "\n")
+    # print("Cluster " + clusters[i] + "\n  " + str(x) + "\n")
+    print(str(x))
 
 
 def getAvailableGPUs_fn(cluster):
-  return (cluster, GPUtil.getAvailable(cluster, limit=4))
+  return (cluster, getAvailable(cluster))
 
-def getAvailableGPUs():
-  p = Pool(len(clusters))
-  gpu_list = p.map(getAvailableGPUs_fn, clusters)
+def getAvailableGPUs(numgpu = 1, custom_clusters=None):
+  if custom_clusters is not None:
+    cs = custom_clusters
+  else:
+    cs = clusters
+
+  p = Pool(len(cs))
+  gpu_list = p.map(getAvailableGPUs_fn, cs)
+
+  # no_power_saving = ["v7", "v8", "v9", "v10", "v23", "v24"]
+  for gpu in gpu_list:
+    random.shuffle(gpu[1])
+    # if gpu[0] not in no_power_saving and len(gpu[1]) > 0:
+      # gpu[1].pop()
+
   gpu_list.sort(key=lambda x:len(x[1]), reverse=True)
-  return (gpu_list[0][0], gpu_list[0][1][0]) # cluster, gpu_id
+  # print(gpu_list)
+
+  print(gpu_list[0][1])
+  if len(gpu_list[0][1]) < numgpu:
+    raise Exception("%d gpus not available" % numgpu)
+
+  return (gpu_list[0][0], gpu_list[0][1][:numgpu]) # cluster, gpu_id
 
 def main():
   if sys.argv[1] == "ls":
@@ -106,47 +131,65 @@ def main():
     os.system("ssh " + sys.argv[2] + " -t \"tmux a\"")
   else:
     if "@" not in sys.argv[1]:
-      print("Wrong format. Needed: SESSION_NAME@ClUSTER[gNUM]")
+      print("Wrong format. Needed: SESSION_NAME@ClUSTER[gID][#NUMGPUs]")
       exit()
 
     sp = sys.argv[1].split("@")
-
     code = sp[1]
+
+    if "#" in code: # user specify multiple gpus
+      sp2 = code.split("#")
+      code = sp2[0]
+      num_gpu = int(sp2[1])
+      if num_gpu < 0 or num_gpu > 4:
+        raise ValueError("num_gpu invalid")
+    else:
+      num_gpu=1
+
     if code == "": #automatically select cluster and gpu
       print("Finding a free cluster and a free gpu...")
-      cluster, gpu_id = getAvailableGPUs()
-      gpu_id=str(gpu_id)
+      cluster, gpu_id = getAvailableGPUs(num_gpu)
+      gpu_id = ",".join([str(x) for x in gpu_id])
     else:
       cluster = ""
-      for c in clusters:
-        if code[:len(c)] == c:
-          cluster = c
-          code = code[len(c):]
-          break
+      if "g" in code:
+        cluster, gpu = code.split("g")
+      else:
+        cluster = code
+        gpu = "a"
 
-      if cluster == "":
+      if cluster not in clusters:
         print("Invalid cluster")
         exit()
-      if code != "" and (code[0] != 'g' or code[1] not in ["0", "1", "2", "3", "a"]):
-        print("Invalid GPU code")
-        exit()
+      # if gpu not in ["0", "1", "2", "3", "a"]:
+        # print("Invalid GPU code")
+        # exit()
 
-      # auto
-      if code == "" or code[1] == "a":
-        print("Finding a free gpu...")
-        gpu_id = str(GPUtil.getFirstAvailable(cluster)[0])
+      if num_gpu == 0:
+        gpu_id = ""
+      elif num_gpu > 1:
+        cluster, gpu_id = getAvailableGPUs(num_gpu, [cluster])
+        gpu_id = ",".join([str(x) for x in gpu_id[:num_gpu]])
       else:
-        gpu_id = code[1]
+        # auto
+        if gpu == "a":
+          print("Finding a free gpu...")
+          gpu_id = str(getFirstAvailable(cluster))
+        else:
+          gpu_id = gpu
 
     print("Using cluster: " + cluster)
     print("Using gpu: " + gpu_id)
 
     print("Establishing session name...")
-    windows = getWindowList(cluster)
+    windows = getWindowList()
     if sp[0] == "":
-      sp[0] = "s00"
+      sp[0] = cluster + "_0"
       while len(windows) and sp[0] in windows:
-        sp[0] = "s%02d" % (int(sp[0][1:])+1)
+        sp[0] = cluster + "_%d" % (int(sp[0].split("_")[-1]) + 1)
+    else:
+      sp[0] = cluster + "_" + sp[0]
+
     session_name = sp[0]
     print("Session name: " + session_name)
 
@@ -154,13 +197,14 @@ def main():
     print("SSHFS Mapping ...")
     user_host = getpass.getuser() + "@" + get_ip()
     target = "~/mnt_tl_" + platform.node() + "/"
-    sshfs_cmd = "ssh " + cluster + " -t \"mkdir -p " + target + "; nohup sshfs -o follow_symlinks -o cache=no -o IdentityFile=~/.ssh/id_rsa " + user_host + ":/ " + target + "\""
+    sshfs_cmd = "ssh " + cluster + " -t \"mkdir -p " + target + "; nohup sshfs -o StrictHostKeyChecking=no -o follow_symlinks -o cache=no -o IdentityFile=~/.ssh/id_rsa " + user_host + ":/ " + target + "\""
     cmd(sshfs_cmd)
 
     tf_cmd = "CUDA_VISIBLE_DEVICES=" + gpu_id + " " + " ".join(sys.argv[2:])
     # terminal_cmd = venv + "; cd " + target + os.getcwd() + "; " + tf_cmd + "; tmux detach"
     terminal_cmd = venv + "; cd " + target + os.getcwd() + "; " + tf_cmd
 
+    print(windows)
     if len(windows) == 0:
       tmux_creation = 'tmux new -A -s ' + session_special + ' -n ' + session_name + '\;'
     elif session_name in windows:
@@ -169,8 +213,15 @@ def main():
     else:
       tmux_creation = 'tmux new -A -s ' + session_special + '\; new-window -t ' + session_special + ' -n ' + session_name + ' \;'
 
-    tmux_cmd = 'ssh ' + cluster + ' -t "' + tmux_creation + ' send-keys \\\" ' + terminal_cmd + '\\\" C-m\;"'
+
+    # https://unix.stackexchange.com/questions/266866/how-to-prevent-ctrlc-to-break-ssh-connection/841125
+    terminal_cmd = ' ssh ' + cluster + ' -t \\\"trap : INT; ' + terminal_cmd + ' ; echo \\\"' + tf_cmd + '\\\" >> ~/.zsh_history; /bin/zsh \\\"; exit' # last exit is for when closing ssh connection, also close ROG
+
+    # tmux_cmd = tmux_creation + ' send-keys "' + terminal_cmd + '" C-m\; splitw -l 4 \; send-keys "' + tf_cmd + '" \; select-pane -P \'bg=colour234 fg=colour72\' \; select-pane -t 1 \;'
+    tmux_cmd = tmux_creation + ' send-keys "' + terminal_cmd + '" C-m\;'
+
     cmd(tmux_cmd)
+    # print(tmux_cmd)
 
 if __name__== "__main__":
   main()
